c48254889a51c4823fc26f16774d63c94a79feae
guilherme <168210658+GUILHERME165@users.noreply.github.com>
2024-11-19 20:32:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,79 @@
+criacatao(
+
+"HQs",
+
+"quem é o homem-aranha?",
+
+"é o peter parker"
+
+)
+
+criacatao(
+
+    "jogos",
+    
+    "quem é o personagem mais rapido no mundo dos jogos?",
+    
+    "sonic o ouriço"
+    
+    )
+
+    criacatao(
+
+        "anime",
+        
+        "qual é a obra considerada o pai dos animes?",
+        
+        "dragon ball"
+        
+        )
+
+        criacatao(
+
+            "programaçao",
+            
+            "o que é CSS?",
+            
+            "o CSS é uma linguagem de estilizaçao"
+            
+            )
+
+            criacatao(
+
+                "geografia",
+                
+                "qual a capital da frança?",
+                
+                "a capital da frança é paris"
+                
+                )
+
+                criacatao(
+
+                    "anime",
+                    
+                    "qual é o anime que criou o shonnen?",
+                    
+                    "astroboy"
+                    
+                    )
+
+                    criacatao(
+
+                        "jogos",
+                        
+                        "qual foi a franquia que salvou a indústria dos jogos da falencia?",
+                        
+                        "super mario bros"
+                        
+                        )
+
+                        criacatao(
+
+                            "HQs",
+                            
+                            "qual é a cidade das hqs de herois é a mias famosa?",
+                            
+                            "gotham city"
+                            
+                            )
